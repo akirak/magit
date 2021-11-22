f6352babe7ea57ea9df4404ec3b32a72cca84654
Akira Komamura <akira.komamura@gmail.com>
2021-11-22 22:58:37 +0900

    Remove unnecessary Nix code

--- flake.nix
+++ flake.nix
@@ -1,21 +1,9 @@
 {
-  description = "Basic project with pre-commit check";
+  description = "A Git porcelain inside Emacs";
 
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
     inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
-  inputs.gitignore = {
-    url = "github:hercules-ci/gitignore.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.emacs-overlay = {
-    url = "github:nix-community/emacs-overlay";
   };
   inputs.emacs-ci = {
     url = "github:akirak/nix-emacs-ci/add-flake";
@@ -27,49 +15,22 @@
     { self
     , nixpkgs
     , flake-utils
-    , pre-commit-hooks
-    , gitignore
-    , emacs-overlay
     , emacs-ci
     }:
     flake-utils.lib.eachDefaultSystem
       (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            emacs-overlay.overlay
-          ];
-        };
-
-        inherit (gitignore.lib) gitignoreSource;
-
-        makeEmacs = version: emacs-ci.packages.${system}.${version};
-
-        emacs = makeEmacs "emacs-27-2";
-      in
-      rec {
+      {
         packages = flake-utils.lib.flattenTree (
-          builtins.mapAttrs (name: emacs:
-            let
-              epkgs = (pkgs.emacsPackagesFor emacs);
-            in
+          builtins.mapAttrs
+            (name: emacs:
+              let
+                epkgs = (nixpkgs.legacyPackages.${system}.emacsPackagesFor emacs);
+              in
               epkgs.magit.overrideAttrs (pkg: {
-                buildInputs = pkg.buildInputs ++ [epkgs.libgit];
+                buildInputs = pkg.buildInputs ++ [ epkgs.libgit ];
               })
-          ) emacs-ci.packages.${system}
+            )
+            emacs-ci.packages.${system}
         );
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = gitignoreSource ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              nix-linter.enable = true;
-            };
-          };
-        };
-        devShell = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
       });
 }
