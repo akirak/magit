dc846ecf495ca9f73649d905b91b0d104d28ba21
Akira Komamura <akira.komamura@gmail.com>
2021-11-22 22:06:23 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Basic project with pre-commit check";
+
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.pre-commit-hooks = {
+    url = "github:cachix/pre-commit-hooks.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.emacs-overlay = {
+    url = "github:nix-community/emacs-overlay";
+  };
+  inputs.emacs-ci = {
+    url = "github:akirak/nix-emacs-ci/add-flake";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , pre-commit-hooks
+    , gitignore
+    , emacs-overlay
+    , emacs-ci
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            emacs-overlay.overlay
+          ];
+        };
+
+        inherit (gitignore.lib) gitignoreSource;
+
+        makeEmacs = version: emacs-ci.packages.${system}.${version};
+
+        emacs = makeEmacs "emacs-27-2";
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree (
+          builtins.mapAttrs (name: emacs:
+             (pkgs.emacsPackagesFor emacs).emacsWithPackages (epkgs: [
+                (epkgs.magit.overrideAttrs (_pkg: {
+                  src = self.sourceInfo.outPath;
+                }))
+                epkgs.libgit
+             ])
+          ) emacs-ci.packages.${system}
+        );
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = gitignoreSource ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              nix-linter.enable = true;
+            };
+          };
+        };
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      });
+}
